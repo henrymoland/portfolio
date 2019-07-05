06028a05de78babfdac21412abc51cbd23ec3066
Henry Moland <molandh@gmail.com>
2019-07-05 12:49:01 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nodemailer = require("nodemailer");
+const path = require('path');
+
+const server = express();
+server.use(express.json());
+
+server.post("/send", (req, res) => {
+    console.log("submited")
+});
+
+server.use("/", routes);
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    server.use(express.static('client/build'));
+    server.get('*', (req, res) => {
+        res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT);
